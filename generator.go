e74c50aa7dcfb9c75b6a1f6c4a059caeaf3e63c1
Yauheni <yauhenbelarus@gmail.com>
2023-12-18 15:03:47 +0300

    size work

--- generator.go
+++ generator.go
@@ -1,1 +1,30 @@
-package placeholder_generator
+package generator
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+func Generate(height, width int, filename string) {
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	c := color.RGBA{R: 128, G: 128, B: 128, A: 255}
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			img.Set(x, y, c)
+		}
+	}
+
+	f, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// Запишите изображение в файл
+	if err := png.Encode(f, img); err != nil {
+		panic(err)
+	}
+}
--- service/web.go
+++ service/web.go
@@ -5,20 +5,54 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	generator "placeholder-generator"
+	"strconv"
 )
 
-func mainHandler(w http.ResponseWriter, _ *http.Request) {
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
 	tmpl, _ := template.ParseFiles("static/templates/index.html")
 	err := tmpl.Execute(w, nil)
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	heightStr := r.URL.Query().Get("height")
+	widthStr := r.URL.Query().Get("width")
+
+	height, err := strconv.Atoi(heightStr)
+	if err != nil {
+		http.Error(w, "Invalid height", http.StatusBadRequest)
+		return
+	}
+
+	width, err := strconv.Atoi(widthStr)
+	if err != nil {
+		http.Error(w, "Invalid width", http.StatusBadRequest)
+		return
+	}
+
+	generator.Generate(height, width, "static/images/placeholder.png")
+	http.ServeFile(w, r, "static/images/placeholder.png")
 }
 
 func notFoundHandler(w http.ResponseWriter, _ *http.Request) {
 	tmpl, _ := template.ParseFiles("static/templates/404.html")
 	err := tmpl.Execute(w, nil)
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
@@ -26,6 +60,7 @@ func notFoundHandler(w http.ResponseWriter, _ *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", mainHandler)
+	r.HandleFunc("/image", imageHandler)
 	r.PathPrefix("/style/").Handler(http.StripPrefix("/style/", http.FileServer(http.Dir("static/style"))))
 	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	err := http.ListenAndServe(":8080", r)
